a6168117e6e259b1ea614bd0c565c659ba064f66
Dallin McLaughlin <nillad12@gmail.com>
2023-11-29 14:28:07 +0800

    Setup basic application and entry point

---
+++ GameEngine/src/GameEngine.h
@@ -0,0 +1,11 @@
+#pragma once
+
+//For use by Hazel applications
+
+#include <stdio.h>
+
+#include "GameEngine/Application.h"
+
+//---------ENTRY POINT-------------
+#include "GameEngine/EntryPoint.h"
+//---------------------------------
